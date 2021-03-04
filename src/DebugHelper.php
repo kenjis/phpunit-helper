24355462608c3028789f58392e0fe8509f799a95
kenjis <kenji.uui@gmail.com>
2021-03-04 10:01:56 +0900

    docs: add @codeCoverageIgnore for codecov.io

--- src/DebugHelper.php
+++ src/DebugHelper.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Kenjis\PhpUnitHelper;
 
-require_once __DIR__ . '/functions.php';
+require_once __DIR__ . '/functions.php'; // @codeCoverageIgnore
 
 trait DebugHelper
 {
